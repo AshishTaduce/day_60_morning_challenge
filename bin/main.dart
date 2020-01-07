82f6ac4c20d8b1eb883a9f113aa1a1bce3bb13c3
AshishTaduce <ashishteddy2@gmail.com>
2020-01-07 12:53:52 +0530

    s

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,48 @@
 
 void main() {
+  print(rugCount([
+    "AAAAAAAAAAA",
+    "AABBBBBBBAA",
+    "AABCCCCCBAA",
+    "AABCAAACBAA",
+    "AABCADACBAA",
+    "AABCAAACBAA",
+    "AABCCCCCBAA",
+    "AABBBBBBBAA",
+    "AAAAAAAAAAA"
+  ])) ;
+}
+
+String chopShop(String input){
+  if (input == ''){return input;}
+  List<String> inputList;
+  inputList = input.split('').toList();
+  String target;
+  target = inputList[0];
+//  print(inputList);
+  while(inputList.isNotEmpty && inputList[0] == target ){
+    inputList.removeAt(0);
+  }
+  while(inputList.isNotEmpty && inputList.last == target ){
+    inputList.removeLast();
+  }
+//  print(inputList);
+  return inputList.join('');
+}
+
+List<String> processList (List<String> inputList){
+  inputList = inputList.map((String word) => word = chopShop(word)).toList();
+  inputList.removeWhere((String word) => word == '');
+  return inputList;
+}
+
+int rugCount(List<String> inputList){
+  int count;
+  count = 0;
+  while(inputList.isNotEmpty){
+  inputList = processList(inputList);
+  ++count;
+}
+//print(inputList);
+  return count;
 }
--- test/day_60_morning_challenge_test.dart
+++ test/day_60_morning_challenge_test.dart
@@ -1,5 +1,66 @@
 import 'package:test/test.dart';
-
+import '../bin/main.dart';
 void main() {
+    test('chopshop', (){
+      expect(chopShop("BBCFADSGSBBBB"), equals("CFADSGS"));
+      expect(chopShop("AAAAAAA"), equals(""));
+      expect(chopShop("CCGAJCCCJCCC"), equals("GAJCCCJ"));
+    });
+
+    test('processList', (){
+      expect(processList([
+        "AAAA",
+        "ABBA",
+        "AAAA"
+      ]), equals(['BB']));
+
+      expect(processList([
+        "AAAAAAAAA",
+        "ABBBBBBBA",
+        "ABBAAABBA",
+        "ABBBBBBBA",
+        "AAAAAAAAA"
+      ]), equals(['BBBBBBB', 'BBAAABB', 'BBBBBBB']));
+
+      expect(processList([
+        "AAAAAAAAAAA",
+        "AABBBBBBBAA",
+        "AABCCCCCBAA",
+        "AABCAAACBAA",
+        "AABCADACBAA",
+        "AABCAAACBAA",
+        "AABCCCCCBAA",
+        "AABBBBBBBAA",
+        "AAAAAAAAAAA"
+      ]), equals(['BBBBBBB', 'BCCCCCB', 'BCAAACB', 'BCADACB', 'BCAAACB', 'BCCCCCB', 'BBBBBBB']));
+    });
+
+    test('rugCount', (){
+      expect(rugCount([
+        "AAAA",
+        "ABBA",
+        "AAAA"
+      ]), equals(2));
+
+      expect(rugCount([
+        "AAAAAAAAA",
+        "ABBBBBBBA",
+        "ABBAAABBA",
+        "ABBBBBBBA",
+        "AAAAAAAAA"
+      ]), equals(3));
+
+      expect(rugCount([
+        "AAAAAAAAAAA",
+        "AABBBBBBBAA",
+        "AABCCCCCBAA",
+        "AABCAAACBAA",
+        "AABCADACBAA",
+        "AABCAAACBAA",
+        "AABCCCCCBAA",
+        "AABBBBBBBAA",
+        "AAAAAAAAAAA"
+      ]), equals(5));
+    });
 
 }
